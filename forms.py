b61dac7869bfbfa184e1fabb6b677ae2e21a4dd1
edgarsjanovskis <edgars.j@e-madona.lv>
2021-04-09 21:11:32 +0300

    Added username validation

--- forms.py
+++ forms.py
@@ -1,22 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired, Email, EqualTo
+from wtforms.validators import InputRequired, Email, ValidationError
 from user import User
 
 
 class UserForm(FlaskForm):
 
     username = StringField(
-        label="Username",
-        validators=[InputRequired(), EqualTo(fieldname=(user for user in User.users_list))]
+        label="Username:",
+        validators=[InputRequired()]
     )
 
     email = StringField(
-        label="E-mail",
+        label="E-mail:",
         validators=[InputRequired(), Email(message="Enter valid email")]
     )
     submit = SubmitField(label="Submit")
 
-
+    def validate_username(self, username):
+        for user in User.users_list:
+            if user.username == self.username.data:
+                raise ValidationError(
+                    f"User with username {self.username.data } already exists!"
+                )
 
 
